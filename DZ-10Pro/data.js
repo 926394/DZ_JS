04fb0c9aa720f37bec85ce3b69f8594c97307c1a
Nikolay <meta1mang@mail.ru>
2024-09-23 23:10:23 +0300

    DZ-10PRO

---
+++ DZ-10Pro/data.js
@@ -0,0 +1,27 @@
+const variableData = `[
+
+  { 
+  "message": "https://i.pinimg.com/originals/86/c3/2a/86c32a700a13a27e5f72d138ec089158.png",
+  "livingOrganisms": "flower",
+  "name": "тюльпан Грейга"
+  },
+  {
+  "message": "https://images.dog.ceo/breeds/leonberg/n02111129_324.jpg",
+  "livingOrganisms": "dog",
+  "name": "Леонбергер"
+  },
+  {
+  "message": "https://images.dog.ceo/breeds/hound-blood/n02088466_6231.jpg",
+  "livingOrganisms": "dog",
+  "name": "Бладхаунд"
+  },
+  {
+  "message": "https://images.dog.ceo/breeds/setter-gordon/n02101006_1440.jpg",
+  "livingOrganisms": "dog",
+  "name": "Шотландский сеттер"
+  }
+
+
+
+]
+  `;
